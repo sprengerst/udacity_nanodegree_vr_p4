afc4e035754f3f1a06c6bf50568ce546f0635e49
stefan <stefansprenger@gmx.at>
2017-04-27 15:45:01 +0200

    Fixed game mechanics

--- Application/Assets/Scripts/lightUp.cs
+++ Application/Assets/Scripts/lightUp.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class lightUp : MonoBehaviour {
-	/*public Material lightUpMaterial;
+	public Material lightUpMaterial;
 	public GameObject gameLogic;
 	private Material defaultMaterial;
 
@@ -28,13 +28,13 @@ public class lightUp : MonoBehaviour {
 		//this.GetComponentInChildren<ParticleSystem>().enableEmission = true; //Turn on particle emmission
 		this.GetComponent<GvrAudioSource>().Play();
 
-		//gameLogic.GetComponent<gameLogic>().playerSelection(this.gameObject);
+		gameLogic.GetComponent<GameLogic>().playerSelection(this.gameObject);
 
 
 	}
 	public void playerSelection() {
-		//GameLogic.GetComponent<gameLogic>().playerSelection(this.gameObject);
-		//this.GetComponent<GvrAudioSource>().Play();
+		gameLogic.GetComponent<GameLogic>().playerSelection(this.gameObject);
+		this.GetComponent<GvrAudioSource>().Play();
 	}
 	public void aestheticReset() {
 		this.GetComponent<MeshRenderer>().material = defaultMaterial; //Revert to the default material
@@ -52,5 +52,5 @@ public class lightUp : MonoBehaviour {
 		patternLightUp ();
 		yield return new WaitForSeconds(duration-.1f);
 		aestheticReset ();
-	}*/
+	}
 }
